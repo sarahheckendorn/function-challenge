003ad0f220b5cd565ef7567f63bf89504db9d919
Sarah Heckendorn <praisinghim@jcangus.com>
2018-07-16 14:53:02 -0600

    committing work-in-progress on strings function

--- functions.js
+++ functions.js
@@ -4,6 +4,19 @@ function hardWare() {
 	let sum = hammer + nail;
 	return sum;
 
-}
+};
 
 console.log(hardWare());
+
+
+function strings(berry, cake, frosting) {
+	let berry = 12;
+	let cake = 23;
+	let frosting = 18;
+	let total = berry + cake + frosting;
+	return "I am making a dessert. It wil cost me: " + total;
+
+};
+
+console.log(strings());
+
