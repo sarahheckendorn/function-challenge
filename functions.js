3f664547e24b4a6fa5debfb40c56ac5cb2402466
Sarah Heckendorn <praisinghim@jcangus.com>
2018-07-16 21:07:09 -0600

    Added comments to explain the code

--- functions.js
+++ functions.js
@@ -1,25 +1,25 @@
-function hardWare() {
-	let hammer = 27;
+function hardWare() { //naming my function
+	let hammer = 27; //assigning numbers to my variables
 	let nail = 13;
-	let sum = hammer + nail;
-	return sum;
+	let sum = hammer + nail; //assigning both variables to my sum variable
+	return sum; //returning sum
 
 };
 
-console.log(hardWare());
+console.log(hardWare()); //calling my function to display it's variables
 
 
-function strings(berry, cake, frosting) {
-	let total = berry + cake + frosting;
-	return "I am making a dessert. It will cost me: " + "$" + total ;
+function strings(berry, cake, frosting) { //naming my function and naming parameters
+	let total = berry + cake + frosting; //assigning parameters to the total
+	return "I am making a dessert. It will cost me: " + "$" + total ; //adding strings
 
 };
 
-console.log(strings(17, 29, 10));
+console.log(strings(17, 29, 10)); // calling function and assigning numbers to my parameters
 
-const numberArray =[27, 32, 18, 24];
-const newArray = numberArray.map(number => number * 3);
-	console.log(newArray);
+const numberArray =[27, 32, 18, 24]; // naming my function and assigning my array
+const newArray = numberArray.map(number => number * 3); //using the map function to store information
+	console.log(newArray); //logging the array to multiply the number parameters by 3
 
 
 
